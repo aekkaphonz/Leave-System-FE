8d7acc12c43bcb7f2a7c1e24f5083c2258ef30ce
aekkaphonz <89302065+aekkaphonz@users.noreply.github.com>
2025-02-18 11:38:28 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LeaveRequestFormComponent } from "./leave-request-form/leave-request-form.component";
-import { LeaveApprovalPageComponent } from "./leave-approval-page/leave-approval-page.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
 import { NavbarComponent } from './navbar/navbar.component';
-import { CalendarComponent } from "./calendar/calendar.component";
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CalendarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
